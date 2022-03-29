76b17f708cfa015bbcfd52d38786dcd38ae4e586
Kiet Luu <kbluu02@gmail.com>
2022-03-29 15:09:39 -0700

    Added playRound and game function

--- javascript/script.js
+++ javascript/script.js
@@ -2,12 +2,39 @@ const selection = ["rock", "paper", "scissors"];
 let computerScore = 0;
 let playerScore = 0;
 
+// Randomize computer selection by random index number for array element
 function computerPlay() {
   const randomIndex = getRandomIndex(selection);
   return selection[randomIndex];
 }
 
-function playRound(playerSelection, computerSelection) {}
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+
+  if (playerSelection === computerSelection) {
+    return `Player Selects: ${playerSelection}\nComputer Selects: ${computerSelection}\nIt's a Tie!`;
+  } else if (
+    (playerSelection === `rock` && computerSelection === `scissors`) ||
+    (playerSelection === `paper` && computerSelection === `rock`) ||
+    (playerSelection === `scissors` && computerSelection === `paper`)
+  ) {
+    return `Player Selects: ${playerSelection}\nComputer Selects: ${computerSelection}\nPlayer Wins! Computer Lose.`;
+  } else {
+    return `Player Selects: ${playerSelection}\nComputer Selects: ${computerSelection}\nComputer Wins! Player Lose.`;
+  }
+}
+
+function game() {
+  for (i = 1; i <= 5; i++) {
+    let playerSelection = prompt(
+      "What's Your Selection? (Rock/Paper/Scissors)",
+      ""
+    );
+    let computerSelection = computerPlay();
+    console.log(`Round: ${i}`);
+    console.log(playRound(playerSelection, computerSelection));
+  }
+}
 
 // Generate a random index number based on array length
 function getRandomIndex(array) {
