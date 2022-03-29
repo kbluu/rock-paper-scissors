d89d1f3ee854855639116b7fcf6567d3c5d0f4a3
Kiet Luu <kbluu02@gmail.com>
2022-03-29 14:44:00 -0700

    Added function to generate random index number

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+const selection = ["rock", "paper", "scissors"];
+let computerScore = 0;
+let playerScore = 0;
+
+function computerPlay() {}
+
+function playRound(playerSelection, computerSelection) {}
+
+// Generate a random index number based on array length
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
