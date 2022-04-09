a33e1dfb3350ce3d3e76fbd00a1e6829596e8a23
Kiet Luu <kbluu02@gmail.com>
2022-04-08 17:59:24 -0700

    added js for rps-ui feature

--- javascript/script.js
+++ javascript/script.js
@@ -12,47 +12,65 @@ function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
 
   if (playerSelection === computerSelection) {
-    return `Player Selects: ${playerSelection}\nComputer Selects: ${computerSelection}\nIt's a Tie!`;
+    return `player selects: ${playerSelection} || computer selects: ${computerSelection} || it's a tie!`;
   } else if (
     (playerSelection === `rock` && computerSelection === `scissors`) ||
     (playerSelection === `paper` && computerSelection === `rock`) ||
     (playerSelection === `scissors` && computerSelection === `paper`)
   ) {
     playerScore++;
-    return `Player Selects: ${playerSelection}\nComputer Selects: ${computerSelection}\nPlayer Wins! Computer Lose.`;
+    return `player selects: ${playerSelection} || computer selects: ${computerSelection} || player wins! computer lose.`;
   } else {
     computerScore++;
-    return `Player Selects: ${playerSelection}\nComputer Selects: ${computerSelection}\nComputer Wins! Player Lose.`;
-  }
-}
-
-function game() {
-  for (i = 1; i <= 5; i++) {
-    let playerSelection = prompt(
-      "What's Your Selection? (Rock/Paper/Scissors)",
-      ""
-    );
-    let computerSelection = computerPlay();
-    console.log(`Round: ${i}`);
-    console.log(playRound(playerSelection, computerSelection));
-  }
-
-  if (playerScore === computerScore) {
-    console.log(
-      `Player Score: ${playerScore}\nComputer Score: ${computerScore}\nIt' a Tie Game!`
-    );
-  } else if (playerScore > computerScore) {
-    console.log(
-      `Player Score: ${playerScore}\nComputer Score: ${computerScore}\nPlayer Wins The Game!`
-    );
-  } else {
-    console.log(
-      `Player Score: ${playerScore}\nComputer Score: ${computerScore}\nComputer Wins The Game!`
-    );
+    return `player selects: ${playerSelection} || computer selects: ${computerSelection} || computer wins! player lose.`;
   }
 }
 
 // Generate a random index number based on array length
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
+}
+
+const buttons = document.querySelectorAll(".selection");
+const resultsEl = document.querySelector(".results");
+const playerScoreEl = document.querySelector(".player-score");
+const computerScoreEl = document.querySelector(".computer-score");
+const winnerEl = document.querySelector(".winner");
+
+buttons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    resultsEl.textContent = playRound(button.id, computerPlay());
+    playerScoreEl.textContent = `player score: ${playerScore}`;
+    computerScoreEl.textContent = `computer score: ${computerScore}`;
+    winnerEl.textContent = showWinner();
+    resetScore();
+  });
+});
+
+function showWinner() {
+  if (computerScore === 5 || playerScore === 5) {
+    if (computerScore === playerScore) {
+      resetGame();
+      return `player score: ${playerScore} | computer score: ${computerScore} | it's a tie game!`;
+    } else if (computerScore > playerScore) {
+      resetGame();
+      return `player score: ${playerScore} | computer score: ${computerScore} | computer wins the game!`;
+    } else if (computerScore < playerScore) {
+      resetGame();
+      return `player score: ${playerScore} | computer score: ${computerScore} | player wins the game!`;
+    }
+  }
+}
+
+function resetScore() {
+  if (computerScore === 5 || playerScore === 5) {
+    playerScore = 0;
+    computerScore = 0;
+  }
+}
+
+function resetGame() {
+  resultsEl.textContent = "";
+  playerScoreEl.textContent = "";
+  computerScoreEl.textContent = "";
 }
